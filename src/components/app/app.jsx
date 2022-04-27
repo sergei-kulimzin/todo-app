85de93ba214afe7466de7c658290ecc8f59dea19
Sergei Kulimzin <kulimzin.sergei@gmail.com>
2022-04-27 16:18:47 +0300

    add route for EditTodoListPage

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -4,12 +4,17 @@ import Layout from '../../pages/layout/layout';
 import CreateTodoListPage from '../../pages/create-todo-list-page/create-todo-list-page';
 import MyTodoListsPage from '../../pages/my-todo-lists-page/my-todo-lists-page';
 import NotFoundPage from '../../pages/not-found-page/not-found-page';
+import EditTodoListPage from '../../pages/edit-todo-list-page/edit-todo-list-page';
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Layout />}>
         <Route index element={<CreateTodoListPage />} />
+        <Route
+          path='edit-todo-list/:todoListID'
+          element={<EditTodoListPage />}
+        />
         <Route path='my-todo-lists' element={<MyTodoListsPage />} />
         <Route path='*' element={<NotFoundPage />} />
       </Route>

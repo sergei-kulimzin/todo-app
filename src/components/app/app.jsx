d6ca5ec5bf4f666f04e5005fc6c4fb29464ca223
Sergei Kulimzin <kulimzin.sergei@gmail.com>
2022-04-27 15:14:26 +0300

    add app routing

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,5 +1,20 @@
+import { Route, Routes } from 'react-router-dom';
+
+import Layout from '../../pages/layout/layout';
+import CreateTodoListPage from '../../pages/create-todo-list-page/create-todo-list-page';
+import MyTodoListsPage from '../../pages/my-todo-lists-page/my-todo-lists-page';
+import NotFoundPage from '../../pages/not-found-page/not-found-page';
+
 function App() {
-  return <h1>App</h1>;
+  return (
+    <Routes>
+      <Route path='/' element={<Layout />}>
+        <Route index element={<CreateTodoListPage />} />
+        <Route path='my-todo-lists' element={<MyTodoListsPage />} />
+        <Route path='*' element={<NotFoundPage />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
+import { BrowserRouter } from 'react-router-dom';
 
 import App from './components/app/app';
 
@@ -10,7 +11,9 @@ const root = createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );

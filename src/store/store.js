391d0af26cec092d0b5c3b694558e5b6e2f5bed9
Kulimzin Sergei <kulimzin.sergei@gmail.com>
2022-05-01 18:01:00 +0300

    fix: redux store, routing

--- src/store/store.js
+++ src/store/store.js
@@ -2,10 +2,9 @@ import { createStore, compose, applyMiddleware } from 'redux';
 import { LocalStorageMiddleware } from '../middleWares/local-storage-middleware';
 import { reducer } from './reducer';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 export const store = createStore(
   reducer,
-  compose(
-    applyMiddleware(LocalStorageMiddleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  composeEnhancers(applyMiddleware(LocalStorageMiddleware))
 );
